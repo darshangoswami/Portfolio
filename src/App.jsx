d98484bffbb51f7518229372be557199f7b3bde0
darshangoswami <goswamidarshan999@gmail.com>
2024-08-15 17:23:29 -0500

    restructure to sperate nav and main

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ import resume from "./assets/darshanGoswami_SWE_Resume.pdf";
 
 function App() {
   return (
-    <div className="px-10 md:px-20">
+    <div>
       <nav className="flex flex-wrap md:flex-nowrap md:justify-between py-5 sticky top-0">
-        <Button variant="link" className="text-2xl pl-0">
+        <Button variant="link" className="text-2xl">
           Darshan.
         </Button>
         <div className="mt-0.5">
-          <Button asChild variant="link" className="text-md pl-0">
+          <Button asChild variant="link" className="text-md">
             <a href="https://github.com/darshangoswami" target="_blank">
               Github
             </a>
@@ -39,57 +39,65 @@ function App() {
           </Button>
         </div>
       </nav>
-      <div className="h-screen flex flex-col justify-center">
-        <h1 className="text-4xl md:text-6xl text-gray-800 font-medium">
-          Hi, I'm Darshan! <span className="text-3xl">👋</span>
-        </h1>
-        <h2 className="text-2xl text-gray-400 md:text-3xl mt-2">
-          I'm a <span className="text-gray-800">Software Engineer</span>, and I
-          like to solve problems.
-        </h2>
-        <a
-          className="flex md:justify-end lg:mr-52 mt-20 text-2xl underline"
-          href="mailto:goswamd@purdue.edu"
-        >
-          Let's work together!
-        </a>
-      </div>
-      <h3 className="text-2xl underline mb-3">Projects:</h3>
-      <div className="flex flex-wrap md:flex-nowrap gap-5 mb-10">
-        <a href="" className="hover:scale-105 transition-all w-full md:w-auto">
-          <Card className="shadow-inner">
-            <CardHeader className="pb-3">
-              <CardTitle>Shufflefy</CardTitle>
-              <CardDescription>A better shuffle for Spotify.</CardDescription>
-            </CardHeader>
-            <CardContent className="w-full md:w-96 h-60 p-2">
-              <img
-                className="m-auto max-h-full rounded-sm"
-                src={shufflefy_screenshot}
-                alt=""
-              />
-            </CardContent>
-          </Card>
-        </a>
+      <main className="px-10 md:px-20">
+        <div className="h-screen flex flex-col justify-center">
+          <h1 className="text-4xl md:text-6xl text-gray-800 font-medium">
+            Hi, I'm Darshan! <span className="text-3xl">👋</span>
+          </h1>
+          <h2 className="text-2xl text-gray-400 md:text-3xl mt-2">
+            I'm a <span className="text-gray-800">Software Engineer</span>, and
+            I like to solve problems.
+          </h2>
+          <a
+            className="flex md:justify-end lg:mr-52 mt-20 text-2xl underline"
+            href="mailto:goswamd@purdue.edu"
+          >
+            Let's work together!
+          </a>
+        </div>
+        <h3 className="text-2xl underline mb-3">Projects:</h3>
+        <div className="flex flex-wrap md:flex-nowrap gap-5 mb-10">
+          <a
+            href=""
+            className="hover:scale-105 transition-all w-full md:w-auto"
+          >
+            <Card className="shadow-inner">
+              <CardHeader className="pb-3">
+                <CardTitle>Shufflefy</CardTitle>
+                <CardDescription>A better shuffle for Spotify.</CardDescription>
+              </CardHeader>
+              <CardContent className="w-full md:w-96 h-60 p-2">
+                <img
+                  className="m-auto max-h-full rounded-sm"
+                  src={shufflefy_screenshot}
+                  alt=""
+                />
+              </CardContent>
+            </Card>
+          </a>
 
-        <a href="" className="hover:scale-105 transition-all w-full md:w-auto">
-          <Card className="shadow-inner">
-            <CardHeader className="pb-3">
-              <CardTitle>Covid-19 Tracker</CardTitle>
-              <CardDescription>
-                A covid-19 statistics dashboard.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="w-full md:w-96 h-60 p-2">
-              <img
-                className="m-auto max-h-full rounded-sm"
-                src={covid_tracker_screenshot}
-                alt=""
-              />
-            </CardContent>
-          </Card>
-        </a>
-      </div>
+          <a
+            href=""
+            className="hover:scale-105 transition-all w-full md:w-auto"
+          >
+            <Card className="shadow-inner">
+              <CardHeader className="pb-3">
+                <CardTitle>Covid-19 Tracker</CardTitle>
+                <CardDescription>
+                  A covid-19 statistics dashboard.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="w-full md:w-96 h-60 p-2">
+                <img
+                  className="m-auto max-h-full rounded-sm"
+                  src={covid_tracker_screenshot}
+                  alt=""
+                />
+              </CardContent>
+            </Card>
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
